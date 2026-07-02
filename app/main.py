17e3f5171c0861e391575b00f8274ff8eb6ab1e7
hello_zhanghao <hello_zhanghao@outlook.com>
2026-07-02 20:17:51 +0800

    chore: bump version to 2.9.1

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ import glob
 from datetime import datetime
 
 # 版本信息
-__VERSION__ = "2.9.0"
+__VERSION__ = "2.9.1"
 __UPDATE_DATE__ = "2026-07-02"
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
